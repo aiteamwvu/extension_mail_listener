0178f88f778e55e482907f45005a468693c24a28
anonymous0010 <ctoxtli@gmail.com>
2017-10-08 15:50:39 -0400

    New changes

--- listener.py
+++ listener.py
@@ -1,4 +1,4 @@
-import pymongo, config
+import pymongo, config, feedparser
 from flask import Flask, request
 from flask_cors import CORS
 app = Flask(__name__)
@@ -7,12 +7,23 @@ conn = pymongo.MongoClient()[config.mongo_db][config.mongo_col]
 
 @app.route("/")
 def index():
-	return "OK"
+	print("new")
 	url = request.args.get("url") if request.args.get("url") else ""
 	category = request.args.get("category") if request.args.get("category") else ""
 	return store(url, category)
 
 def store(url, category):
-	return '{"status":"OK"}'
+	feed = feedparser.parse(url)
+	if len(feed["entries"]) > 0:
+		conn.save({
+			"_id": url,
+		    "source_table": "Article",
+			"source_name": feed["feed"]["title"],
+			"source_url": url,
+			"source_content": "text",
+			"source_categories": category.split(",")
+		})
+		return '{"success": true}'
+	return '{"success": false}'
 
-app.run(host='0.0.0.0', port=6000, threaded=True)
+app.run(host='0.0.0.0', port=5001, threaded=True)
