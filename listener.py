9c8bc3108bb401568d26627665bebeadc3826506
saviaga <saviaga@gmail.com>
2017-10-11 10:44:41 -0500

    Update listener.py

--- listener.py
+++ listener.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
-conn = pymongo.MongoClient()[config.mongo_db][config.mongo_col]
+conn = pymongo.MongoClient()[config.mongo_db]
 
 @app.route("/")
 def index():
@@ -11,10 +11,78 @@ def index():
 	category = request.args.get("category") if request.args.get("category") else ""
 	return store(url, category)
 
+def store_email_url(url):
+    conn[config.article].save({
+   "media content": [{"url": "URL_IMG"}],
+   "_id":url,
+   "source_name":domain,
+   "links":[
+      {
+         "href":url,
+         "type":"text/html",
+         "rel":""
+      }
+   ],
+   "source_table":"Article",
+   "wfw_commentrss":"",
+   "id":url,
+   "slash_comments":" ",
+   "published_parsed":"",
+   "author":"",
+   "comments":"",
+   "content":[
+      {
+         "base":"",
+         "type":"text/html",
+         "value":"",
+         "language":null
+      }
+   ],
+   "title_detail":{
+      "base":"",
+      "type":"text/plain",
+      "value":"",
+      "language":null
+   },
+   "summary_detail":{
+      "base":"",
+      "type":"text/html",
+      "value":"",
+      "language":null
+   },
+   "tags":[
+      {
+         "term":"General",
+         "scheme":null,
+         "label":null
+      }
+   ],
+   "timestamp":int(time.mktime(parse(article["published"]).timetuple())),
+   "source_url":"",
+   "link":"url",
+   "authors":[
+      {
+         "name":""
+      }
+   ],
+   "author_detail":{
+      "name":""
+   },
+   "source_categories":[
+      ""
+   ],
+   "source_content":"text",
+   "summary":summary,
+   "guidislink":false,
+   "published":"",
+   "title":""
+}
+)
+
 def store(url, category):
 	feed = feedparser.parse(url)
 	if len(feed["entries"]) > 0:
-		conn.save({
+		connconn[config.mongo_col].save({.save({
 			"_id": url,
 		    "source_table": "Article",
 			"source_name": feed["feed"]["title"],
