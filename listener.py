6993163bce57330181ba1c5603e16b952783157f
saviaga <saviaga@gmail.com>
2017-10-18 15:41:36 -0500

    Update listener.py

--- listener.py
+++ listener.py
@@ -136,8 +136,7 @@ def get_info_url(url):
             image=image['src']
         except:
             pass
-	if len(title)==0:
-            title=allcontent[0:50] #if there is no title grab first characters of text
+
 
         return  allcontent,  domain, title,image
     except Exception as e:
